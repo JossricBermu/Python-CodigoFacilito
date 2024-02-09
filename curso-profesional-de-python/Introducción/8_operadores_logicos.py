c3bcbdf0173ec765ca27d273b5e18bdf7abe3d42
JossricBermu <josebermeodev@gmail.com>
2024-02-09 12:10:34 -0500

    Creado operadores logicos

---
+++ curso-profesional-de-python/Introducción/8_operadores_logicos.py
@@ -0,0 +1,13 @@
+# and, or y not
+
+resultado_final = True and True and 100 > 20
+print(resultado_final)
+
+resultado_final = False or False or 10 > 20
+print(resultado_final)
+
+resultado_final = True and (False or 50 > 10)
+print(resultado_final)
+
+resultado_final = not False
+print(resultado_final)
