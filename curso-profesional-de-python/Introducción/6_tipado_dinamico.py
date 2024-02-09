d0046ae2fe05024e75cb92203af678dd7d9c287d
JossricBermu <josebermeodev@gmail.com>
2024-02-09 12:09:27 -0500

    Creado operadores relacionales

---
+++ curso-profesional-de-python/Introducción/6_tipado_dinamico.py
@@ -0,0 +1,19 @@
+valor = "Eduardo"
+print(valor)
+print(type(valor))
+
+valor = 2
+print(valor)
+print(type(valor))
+
+valor = 3.1
+print(valor)
+print(type(valor))
+
+valor = True
+print(valor)
+print(type(valor))
+
+valor = 'CódigoFacilito'
+print(valor)
+print(type(valor))
---
+++ curso-profesional-de-python/Introducción/7_operadores_relacionales.py
@@ -0,0 +1,14 @@
+numero_uno = 20
+numero_dos = 50
+
+"""
+    >
+    <
+    >=
+    <=
+    ==
+    !=
+"""
+
+resultado = numero_uno == numero_dos
+print(resultado)
