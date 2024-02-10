ef33b4d6712b8ff0e3776f9ae7b3368f21261c16
JossricBermu <josebermeodev@gmail.com>
2024-02-10 10:48:59 -0500

    Actualizado entrada.py

--- curso-profesional-de-python/Introducción/9_entrada.py
+++ curso-profesional-de-python/Introducción/9_entrada.py
@@ -1,4 +1,19 @@
 nombre_completo = input('Ingresa tu nombre completo: ') # str
 
+edad = int(input('Ingresa tu edad: '))
+
+altura = float(input('Ingresa tu altura: '))
+
+autorizacion = input('¿Atorizas el programa? (si / no) ') == 'si'
+
 print(nombre_completo)
 print(type(nombre_completo))
+
+print(edad)
+print(type(edad))
+
+print(altura)
+print(type(altura))
+
+print(autorizacion)
+print(type(autorizacion))
