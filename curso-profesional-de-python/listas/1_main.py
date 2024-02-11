505b9e8b4ac47d80f95f6515195259c905a6e50b
JossricBermu <josebermeodev@gmail.com>
2024-02-11 14:40:17 -0500

    Creado main

---
+++ curso-profesional-de-python/listas/1_main.py
@@ -0,0 +1,12 @@
+#            0      1    2     3
+lista = ['String', 10, 15.6, True] # list()
+
+print(lista)
+
+lista_strings = ["Eduardo", "Código", "Curso Python"]
+
+lista_enteros = [-19, 0, 10, 200]
+
+lista_floats = [19.5, 20.5, 80.99, 100.0]
+
+lista_booleanos = [True, True, (2 < 10), (4 > 3 and 10 != 11)]
