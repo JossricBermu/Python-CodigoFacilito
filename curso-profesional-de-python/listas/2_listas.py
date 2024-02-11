0a84310ba228a68e69d4e1b559a7601e40b23aec
JossricBermu <josebermeodev@gmail.com>
2024-02-11 14:41:05 -0500

    Creado listas

---
+++ curso-profesional-de-python/listas/2_listas.py
@@ -0,0 +1,14 @@
+#                   0        1         2       3        4
+#                  -5       -4        -3      -2       -1
+lista_cursos = ['Python', 'Django', 'Flask', 'Ruby', 'Java']
+
+primer_curso = lista_cursos[0]
+print(primer_curso)
+
+segundo_curso = lista_cursos[1]
+print(segundo_curso)
+
+ultimo_curso = lista_cursos[4]
+print(ultimo_curso)
+
+ultimo_curso = lista_cursos[-1]
