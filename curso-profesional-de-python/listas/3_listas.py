f65aae1017cc3165491e370dc9de776b82e79186
JossricBermu <josebermeodev@gmail.com>
2024-02-13 17:16:02 -0500

    Creado listas

---
+++ curso-profesional-de-python/listas/3_listas.py
@@ -0,0 +1,5 @@
+#                   0        1         2       3        4
+#                  -5       -4        -3      -2       -1
+lista_cursos = ['Python', 'Django', 'Flask', 'Ruby', 'Java']
+
+lista_cursos[0] = 'Rust'
