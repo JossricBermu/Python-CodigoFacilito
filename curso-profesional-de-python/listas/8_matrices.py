9dbad94572936d9c048aa62edd4103fb58333866
JossricBermu <josebermeodev@gmail.com>
2024-02-20 16:01:03 -0500

    Creado matrices

---
+++ curso-profesional-de-python/listas/8_matrices.py
@@ -0,0 +1,9 @@
+columna_a = [10, 20, 30, 40]
+columna_b = [30, 40, 50, 60]
+
+matriz = [columna_a, columna_b] # 2 x 4
+
+print(matriz[0][0])
+print(matriz[1][0])
+print(matriz[1][1])
+print(matriz[1][3])
