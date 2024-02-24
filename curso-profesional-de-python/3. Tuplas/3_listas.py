965167cd6997f8e41eedce8f2f74fb817b871b8a
JossricBermu <josebermeodev@gmail.com>
2024-02-24 12:36:17 -0500

    Creado listas

---
+++ curso-profesional-de-python/3. Tuplas/3_listas.py
@@ -0,0 +1,15 @@
+cursos = ['Python', 'Django', 'Flask']
+
+# Generar una tupla
+cursos_tupla = tuple(cursos)
+
+print(cursos_tupla)
+print(type(cursos_tupla))
+
+niveles = ('Básico', 'Intermedio', 'Avanzado')
+
+# Generar una lista
+niveles_lista = list(niveles)
+
+print(niveles_lista)
+print(type(niveles_lista))
