bf83f39fe4ca2cbbb270265143ceddd9c2de3d4d
JossricBermu <josebermeodev@gmail.com>
2024-02-27 15:23:22 -0500

    Creado strings

---
+++ curso-profesional-de-python/4. Strings/strings.py
@@ -0,0 +1,21 @@
+"""
+lenguajes = 'Python Ruby Java Rust C++ C'
+
+listado_lenguajes = lenguajes.split() # espacios
+
+print(listado_lenguajes)
+
+
+lenguajes = 'Python/-/*Ruby/-/*Java/-/*Rust/-/*C++/-/*C'
+
+listado_lenguajes = lenguajes.split('/-/*', 2) # espacios
+
+print(listado_lenguajes)
+"""
+
+lenguajes = ['Python', 'Ruby', 'Java', 'Rust']
+
+string_lenguajes = ' '.join(lenguajes)
+
+print(string_lenguajes)
+print(type(string_lenguajes))
