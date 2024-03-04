0f8a51a7f86a455611cb547127919b4a1497dcab
JossricBermu <josebermeodev@gmail.com>
2024-03-04 17:27:55 -0500

    Actualización de concatenar

--- curso-profesional-de-python/4. Strings/2_concatenar.py
+++ curso-profesional-de-python/4. Strings/2_concatenar.py
@@ -5,9 +5,14 @@ apellido = 'García'
 
 # nombre_completo = 'Mr. %s %s %s.' %(nombre, apellido, 'Pérez')
 
+'''
 nombre_completo = 'Mr. {nombre} {primer_apellido} {segundo_apellido}.'.format(
     nombre = nombre,
     primer_apellido = apellido,
     segundo_apellido = 'Pérez'
 )
+'''
+
+nombre_completo = f'Mr. {nombre} {apellido} {"Pérez"}'
+
 print(nombre_completo)
