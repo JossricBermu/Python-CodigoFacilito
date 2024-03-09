07eebacf018c701cebfc7b68ec0501be9d91d65e
JossricBermu <josebermeodev@gmail.com>
2024-03-08 21:10:24 -0500

    Creado alineación

---
+++ curso-profesional-de-python/4. Strings/5_alineacion.py
@@ -0,0 +1,11 @@
+mensaje = 'Hola mundo!'
+
+# ljust -> Justificar a la izquierda
+# rjust -> Justificar a la derecha
+# center -> Centrar
+
+# mensaje = mensaje.ljust(20)
+# mensaje = mensaje.rjust(20)
+mensaje = mensaje.center(20)
+
+print(mensaje)
