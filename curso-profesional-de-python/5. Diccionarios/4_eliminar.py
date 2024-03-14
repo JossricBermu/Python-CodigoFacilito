f098c6e775cde6496250890c5f93cec368a35390
JossricBermu <josebermeodev@gmail.com>
2024-03-13 19:53:14 -0500

    Creado eliminar

---
+++ curso-profesional-de-python/5. Diccionarios/4_eliminar.py
@@ -0,0 +1,14 @@
+diccionario = {'a':1, 'b':2, 'c':3, 'd':4}
+
+print(len(diccionario))
+
+del diccionario['a'] # 1
+valor = diccionario.pop('b') # 2
+
+# Elimina todos los elementos del diccionario
+diccionario.clear()
+
+print(valor)
+
+print(diccionario)
+print(len(diccionario))
