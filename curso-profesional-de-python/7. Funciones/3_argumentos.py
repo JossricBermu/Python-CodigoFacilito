2887800119638d2037f4c932c21889f10d45bc74
JossricBermu <josebermeodev@gmail.com>
2024-03-27 17:02:10 -0500

    Actualizado argumentos

--- curso-profesional-de-python/7. Funciones/3_argumentos.py
+++ curso-profesional-de-python/7. Funciones/3_argumentos.py
@@ -7,6 +7,7 @@ def promedio(*args):
 resultado = promedio(10, 10, 5, 7, 10)
 print(resultado)
 """
+"""
 def promedio(*args):
     return sum(args) / len(args)
 
@@ -18,3 +19,21 @@ def combinacion(p1, p2, *args, p4=500):
     print(p4)
 
 combinacion(10, 20, 1, 2, 3, 4, 5, 6, 7, 8, p4=1000)
+"""
+def promedio(*args): # Tupla
+    return sum(args) / len(args)
+
+
+def usuarios(**kwargs): # Dict
+    print(kwargs)
+    print(type(kwargs))
+
+
+def combinacion(*args, **kwargs):
+    print(args)
+    print(kwargs)
+
+
+# usuarios(eduardo=[10, 10, 8], fernando=[10, 9, 9])
+
+combinacion(1, 2, 3, 4, 5, cody=True, curso='Python')
