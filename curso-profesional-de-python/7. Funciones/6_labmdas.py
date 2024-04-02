674cd44e4e74b56921f8affe6445dacc536ffd76
JossricBermu <josebermeodev@gmail.com>
2024-04-02 18:03:28 -0500

    Creado labmdas

---
+++ curso-profesional-de-python/7. Funciones/6_labmdas.py
@@ -0,0 +1,13 @@
+def centigrados_a_farhenheit(grados):
+    return grados * 1.8 + 32
+
+
+# print(centigrados_a_farhenheit(10))
+# print(centigrados_a_farhenheit(-1))
+# print(centigrados_a_farhenheit(8))
+
+mi_funcion = centigrados_a_farhenheit
+
+print(type(mi_funcion))
+
+print(mi_funcion(10))
