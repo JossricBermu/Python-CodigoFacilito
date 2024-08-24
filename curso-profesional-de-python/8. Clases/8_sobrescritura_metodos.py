fdc3f5614e6bb6f2658a979ad7226a50ce3e5139
JossricBermu <josebermeodev@gmail.com>
2024-08-24 09:38:39 -0500

    Creado sobre escritura de métodos

---
+++ curso-profesional-de-python/8. Clases/8_sobrescritura_metodos.py
@@ -0,0 +1,34 @@
+class SerVivo():
+
+    def dormir(self):
+        print('El ser deurme.')
+
+class Animal(SerVivo): # Clase Padre
+
+    def comer(self):
+        print('El animal come.')
+
+
+class Mascota(Animal): # Clase Padre
+    
+    def comer(self):
+        print('La mascota come.')
+    
+class Felino:
+
+    def cazar(self):
+        print('El felino caza.')
+
+class Gato(Mascota, Felino):
+    
+    def __init__(self, nombre):
+        self.nombre = nombre
+
+    def comer(self):
+        print(f'{self.nombre} come.')
+
+
+patricio = Gato('Patricio')
+
+patricio.comer()
+patricio.dormir()
