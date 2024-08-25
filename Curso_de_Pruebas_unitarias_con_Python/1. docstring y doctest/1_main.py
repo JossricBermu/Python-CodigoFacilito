5d303df0896c7fe98e6ca400217117ca17f39695
JossricBermu <josebermeodev@gmail.com>
2024-08-25 07:19:15 -0500

    Creado comentarios

---
+++ Curso_de_Pruebas_unitarias_con_Python/1. docstring y doctest/1_main.py
@@ -0,0 +1,14 @@
+# Nos permite comentar una sola línea
+
+"""Este comentario
+posee
+saltos de línea.
+'''Podemos utilizar triple comillas simple'''
+"""
+
+'''Este comentario
+también
+posee
+saltos de línea.
+"""Podemos utilizar triple comillas dobles"""
+'''
